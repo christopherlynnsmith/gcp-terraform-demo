ea9f2e5e2431fe7a2aabec3d3074cdccdbaad5cc
Christopher Smith <christopher.lynn.smith@gmail.com>
2023-06-20 00:09:08 -0400

    removed duplicate variable

--- variables.tf
+++ variables.tf
@@ -3,10 +3,3 @@ variable "project_id" {
   type        = string
   default     = "seed-390122"
 }
-
-
-variable "project_id" {
-  description = "Google Project ID."
-  type        = string
-  default     = "seed-390122"
-}
