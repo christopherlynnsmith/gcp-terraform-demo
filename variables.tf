31229dda8e16837618fc829f5266d5e25f393f4f
Christopher Smith <christopher.lynn.smith@gmail.com>
2023-06-19 23:51:46 -0400

    inital checkin

--- variables.tf
+++ variables.tf
@@ -3,3 +3,10 @@ variable "project_id" {
   type        = string
   default     = "seed-390122"
 }
+
+
+variable "project_id" {
+  description = "Google Project ID."
+  type        = string
+  default     = "seed-390122"
+}
